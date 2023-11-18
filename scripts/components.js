405d51abea3662ee92b8089609cadf6bd7186429
Alberto Di Cagno <adicagno@adobe.com>
2023-11-18 13:34:05 +0100

    feat: parent auto-sensing; commit hook; refactoring base component classes

--- scripts/components.js
+++ scripts/components.js
@@ -1,81 +1,4 @@
-// eslint-disable-next-line max-classes-per-file
-class SubBlock extends HTMLElement {
-  // eslint-disable-next-line no-useless-constructor
-  constructor() {
-    super();
-    this.allowedParents = [/^block-/g];
-  }
-
-  hasAllowedParents() {
-    const parentTag = this.parentElement.tagName.toLowerCase();
-    // eslint-disable-next-line no-restricted-syntax
-    for (const allowedParent of this.allowedParents) {
-      if (allowedParent.test(parentTag)) return true;
-    }
-    return false;
-  }
-
-  performPreflightChecks() {
-    const isParenthoodChecked = this.hasAllowedParents();
-    if (!isParenthoodChecked) {
-      // eslint-disable-next-line no-console
-      console.error(`${this.tagName} has wrong parenthood`);
-    }
-  }
-}
-
-class Row extends SubBlock {
-  // eslint-disable-next-line no-useless-constructor
-  constructor() {
-    super();
-    this.allowedParents = [/^block-column$/g, /^block-/g];
-  }
-}
-
-class Column extends SubBlock {
-  // eslint-disable-next-line no-useless-constructor
-  constructor() {
-    super();
-    this.allowedParents = [/^block-row$/g, /^block-/g];
-  }
-
-  render() {
-    this.performPreflightChecks();
-  }
-}
-
-class Wrapper extends SubBlock {
-  // eslint-disable-next-line no-useless-constructor
-  constructor() {
-    super();
-    this.allowedParents = [/^block-row$/g, /^block-/g];
-  }
-
-  render() {
-    this.performPreflightChecks();
-  }
-}
-
-export const StandardElementDefs = {
-  row: Row,
-  column: Column,
-  wrapper: Wrapper,
-};
-
-class ContentWrapperComponent extends HTMLElement {
-  // eslint-disable-next-line no-useless-constructor
-  constructor() {
-    super();
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  render() {
-  }
-}
-
-export class DefaultContentWrapper extends ContentWrapperComponent {
-
-}
+/* eslint-disable max-classes-per-file */
 
 // eslint-disable-next-line no-unused-vars
 export function preRender($this) {
@@ -101,5 +24,100 @@ export function preConnected() {
 }
 
 export function init($this) {
-  $this.attachShadow({ mode: 'open' });
+  if (!$this.shadowRoot) {
+    $this.attachShadow({ mode: 'open' });
+  } else {
+    // eslint-disable-next-line no-console
+    console.warn(`shadowRoot already attached for ${$this.tagName}`);
+  }
+}
+
+// eslint-disable-next-line max-classes-per-file
+class SubBlock extends HTMLElement {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+    init(this);
+    this.allowedParents = [/-brick$/g];
+  }
+
+  connectedCallback() {
+    this.performPreflightChecks();
+  }
+
+  appendChild(node) {
+    this.shadowRoot.appendChild(node);
+  }
+
+  set innerHTML(content) {
+    // eslint-disable-next-line no-console
+    console.log('content', content);
+    // Manipulate shadow root's innerHTML
+    this.shadowRoot.innerHTML = `<div>${content}</div>`;
+  }
+
+  checkParent() {
+    if (!this.parentElement) return false;
+    const parentTag = this.parentElement.tagName.toLowerCase();
+    return this.allowedParents.some((regex) => regex.test(parentTag));
+  }
+
+  performPreflightChecks() {
+    const isParenthoodChecked = this.checkParent();
+    if (!isParenthoodChecked) {
+      // eslint-disable-next-line no-console
+      if (!this.parentElement) {
+        // eslint-disable-next-line no-console
+        console.error('no ancestor found');
+      } else {
+        // eslint-disable-next-line no-console
+        console.error(`${this.tagName} has a wrong ancestor: ${this.parentElement.tagName}, allowed: ${this.allowedParents.map((e) => e.toString()).join(',')}`);
+      }
+    }
+  }
+}
+
+class Row extends SubBlock {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+    this.allowedParents = [/^column-brick$/g, /-brick$/g];
+  }
+}
+
+class Column extends SubBlock {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+    this.allowedParents = [/^row-brick$/g, /-brick$/g];
+  }
+}
+
+class Wrapper extends SubBlock {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+    this.allowedParents = [/^section$/g, /-brick$/g];
+  }
+}
+
+export const StandardElementDefs = {
+  row: Row,
+  column: Column,
+  wrapper: Wrapper,
+};
+
+class ContentWrapperComponent extends HTMLElement {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+  }
+}
+
+export class DefaultContentWrapper extends ContentWrapperComponent {
+
 }
